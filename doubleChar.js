2c746ee1040261aa0d6c1583bb8581fc0402f696
nfatah <nfatah41@gmail.com>
2016-04-01 18:18:01 +0300

    initial commit

--- doubleChar.js
+++ doubleChar.js
@@ -1,4 +1,9 @@
-var numArray;
-var tribonacci = function(numArray){
-  
-};
+function doubleChar(str) {
+  // Your code here
+  var Arr = [];
+  for(var i = 0; i <= str.length-1; i++){
+  Arr.push(str[i]);
+  Arr.push(str[i]);
+  }
+  return Arr.join('');
+}
